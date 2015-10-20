2f9799a344d73f2bcbfa6e1401bc331668b2d3c8
Or Sahar <ors@qmerce.com>
2015-10-20 10:34:31 +0300

    changed files structure

--- app/scripts/gamesApp.js
+++ app/scripts/gamesApp.js
@@ -1,28 +1,17 @@
 var gamesApp = angular.module('gamesApp', ['ngRoute', 'ngResource']);
-//
-//gamesApp.run(function($rootScope, $http) {
-//    $rootScope.isAuthenticated = true;
-//    //$rootScope.current_user = '';
-//    //$rootScope.signout = function() {
-//    //    $http.get('/auth/signout');
-//    //    $rootScope.isAuthenticated = false;
-//    //    $rootScope.current_user = '';
-//    //};
-//});
 
 var baseUrl = 'http://localhost:3000';
 
 gamesApp.config(function($routeProvider) {
     $routeProvider
 
-        //main page
         .when('/', {
-            templateUrl: 'main.html',
+            templateUrl: 'views/main.html',
             controller: 'mainController'
         })
 
         .when('/#', {
-            templateUrl: 'main.html',
+            templateUrl: 'views/main.html',
             controller: 'mainController'
         })
 
@@ -32,33 +21,12 @@ gamesApp.factory('postService', function($resource) {
     return $resource(baseUrl+'/api/posts/:id');
 });
 
-
-
 gamesApp.controller('mainController', function(postService, $scope,$http){
     $scope.posts = postService.query();
     $scope.editMode = false;
     //set default to 1
     $scope.counter = 1;
     $scope.images = [0];
-    $scope.union = function(a,b){
-        var c = a;
-        var exist = false;
-        for(var i = 0; i<=b.length;i++){
-            for(var j = 0;j<= a.length; j++)
-            {
-                if(b[i] === a[j])
-                {
-                    exist = true;
-                }
-            }
-            if(!exist)
-            {
-                c.push(b[i]);
-            }
-            exist = false;
-        }
-        return c;
-    };
     $scope.newPost = {created_by: '', name: '',description:'', created_at: '', images: []};
     $scope.addOption = function(){
         $scope.images.push($scope.counter);
